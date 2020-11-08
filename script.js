deff3bdb8ecf3038032029ba6e71d574cca38d6e
Victoria Konkiel <victoria.konkiel@yahoo.com>
2020-11-08 14:52:51 -0500

    this is my first commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const numberButtons = document.querySelectorAll('[data-number]');
 
+numberButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        alert(button.innerText);
+    })
+})
+
+const operationButtons = document.querySelectorAll('[data-operation]');
+
+operationButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        alert(button.innerText);
+    })
+})
